de225aee351ea7458cf47a96ccb228076f4607d3
david <dricciodeabreu1@gmail.com>
2024-10-13 15:11:06 +0100

    Tarea 4

---
+++ PHP/tarea4/index.php
@@ -0,0 +1,72 @@
+<?php
+$servername = "localhost"; // El servidor somos nosotros es el local
+$username = "root"; // El usuario
+$password = "180821"; // La contraseña para acceder al administrador. Esto no es seguro hacerlo pero no pasa nada si es solo una prueba
+$dbname = "tarea1"; // Nombre de la base de datos
+
+// Creamos la conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificamos la conexión
+if ($conn->connect_error) {
+    die("Conexión no completada: " . $conn->connect_error);
+}
+
+// Insertamos el usuario
+if (isset($_POST['insert'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO users (nombre, email) VALUES ('$name', '$email')"; // La sentencia SQL de inserción
+    if (mysqli_query($conn, $sql) === TRUE) { // Es recomendable que verifiquemos si hay errores
+        echo "Nuevo usuario creado exitosamente";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Mostramos los usuarios
+if (isset($_POST['select'])) {
+    $sql = "SELECT * FROM users";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo "ID: " . $row["id"]. " - Nombre: " . $row["nombre"]. " - Email: " . $row["email"]. "<br>";
+        }
+        echo '<br><button onclick="window.location.href=\'index.html\'">Volver al administrador de consultas</button>';
+    } else {
+        echo "0 resultados";
+    }
+}
+
+// Actualizamos los usuarios
+if (isset($_POST['update'])) {
+    $userId = $_POST['userId'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $sql = "UPDATE users SET nombre='$name', email='$email' WHERE id=$userId"; // La sentencia SQL de actualizción
+    if (mysqli_query($conn, $sql) === TRUE) {
+        echo "Usuario actualizado exitosamente";
+        echo '<br><button onclick="window.location.href=\'index.html\'"></button>';
+    } else {
+        echo "Error actualizando usuario: " . $conn->error;
+    }
+}
+
+// Borramos el usuario
+if (isset($_POST['delete'])) {
+    $userId = $_POST['userId'];
+
+    $sql = "DELETE FROM users WHERE id=$userId"; // La sentencia SQL de eliminación
+    if (mysqli_query($conn, $sql) === TRUE) {
+        echo "Usuario borrado exitosamente";
+        echo '<br><button onclick="window.location.href=\'index.html\'"></button>';
+    } else {
+        echo "Error borrando usuario: " . $conn->error;
+    }
+}
+
+mysqli_close();
+?>
---
+++ PHP/tarea4/readme.md
@@ -0,0 +1,46 @@
+# Interfaces de contacto con los ficheros php
+
+## Hacer que el HTML funcione con el método POST.
+Para hacer esto, creamos un formulario por operación que realizaremos en la base de datos
+Vamos a ver como sería esto.
+```html
+<form action="index.php" method="post">
+        <input type="text" name="name" placeholder="Name" required>
+        <input type="email" name="email" placeholder="email" required>
+        <button type="submit" name="insert">Crear usuario</button>
+    </form>
+
+    <form action="index.php" method="post">
+        <button type="submit" name="select" id="selectButton">Mostrar usuarios</button>
+    </form>
+
+    <form action="index.php" method="post">
+        <input type="number" name="userId" placeholder="User ID" required>
+        <input type="text" name="name" placeholder="Name" required>
+        <input type="email" name="email" placeholder="email" required>
+        <button type="submit" name="update">Actualizar</button>
+    </form>
+
+    <form action="index.php" method="post">
+        <input type="number" name="userId" placeholder="User ID" required>
+        <button type="submit" name="delete">Borrar </button>
+    </form>
+```
+Wn estos 4 formularios se hace la conexión al index.php  post y submit botón.
+ cada uno de los botones tienen un `name` diferente. Esto es lo que usaremos para que el index.php
+pueda reconocer la solicitud.<br>
+ ahora nuestro objetivo es recoger esto en el index.php mediante `if`, y una vez el código entre
+por una de estas sentencias  realizaremos la recogida de los valores
+```php
+if (isset($_POST['insert'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO users (nombre, email) VALUES ('$name', '$email')"; // La sentencia SQL de inserción
+    if (mysqli_query($conn, $sql) === TRUE) { // Es recomendable que verifiquemos si hay errores
+        echo "Nuevo usuario creado exitosamente";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+```
